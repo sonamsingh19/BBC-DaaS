414625d5ad4b7085fd96ffb4ace544746c9cfe4d
sonamsingh19 <sonamsingh19@gmail.com>
2012-10-18 14:20:33 +0200

    fixed bbcdass to bbcdaas package issue

--- src/de/bbcdaas/opendata/gwt/client/resources/Resources.java
+++ src/de/bbcdaas/opendata/gwt/client/resources/Resources.java
@@ -4,12 +4,9 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
 
-
 public interface Resources extends ClientBundle {
 	public static final Resources INSTANCE = GWT.create(Resources.class);
-	
+
 	@Source("upload.jpg")
 	ImageResource uploadBtnIcon();
-	
-	
 }
